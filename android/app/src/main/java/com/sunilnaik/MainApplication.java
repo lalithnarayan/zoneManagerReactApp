b2d84be56089817071264c2e0a31ef8084ed2c23
Tung Duong <duongtungls@gmail.com>
2019-10-22 22:06:11 +0700

    Fix Android build release

--- android/app/src/main/java/com/sunilnaik/MainApplication.java
+++ android/app/src/main/java/com/sunilnaik/MainApplication.java
@@ -3,6 +3,7 @@ package com.sunilnaik;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.imagepicker.ImagePickerPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ImageResizerPackage(),
             new RNGestureHandlerPackage(),
             new ImagePickerPackage(),
             new VectorIconsPackage(),
