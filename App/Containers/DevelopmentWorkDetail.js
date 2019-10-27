0c7e17cebe45508e88b722779038a3bb6283d32d
Lalith Narayan Kashyap <lalith@dataglen.com>
2019-10-27 16:20:51 +0530

    commented code is removed

--- App/Containers/DevelopmentWorkDetail.js
+++ App/Containers/DevelopmentWorkDetail.js
@@ -37,7 +37,6 @@ class DevelopmentWorkDetail extends Component {
       createdDate: data.created_at ? format(new Date(data.created_at), 'DD-MM-YYYY') : 'NA',
       lastUpdatedAt: data.updated_at ? format(new Date(data.updated_at), 'DD-MM-YYYY') : 'NA',
       metaData: [
-        // { title: 'ಸ್ಥಳ', description: data.place },
         { title: 'ಕೆಲಸದ ವಿಧ', description: data.type_of_work },
         { title: 'ಮಂಜೂರಾದ ಮೊತ್ತ', description: data.sanctioned_amount, hasIcon: true, iconName: 'cash-multiple', isCash: true, },
         { title: 'ಹಾಲಿ ಸ್ಥಿತಿ', description: data.status },
