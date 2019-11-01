a9e1ea9abbd39e453177591f9aa6cf6e3f01db7f
Vishrut Jha <vishrut.jha@nighthack.in>
2019-11-01 17:58:20 +0530

    Push notification changes - OneSignal

--- App/Containers/App.js
+++ App/Containers/App.js
@@ -1,11 +1,13 @@
-import '../Config'
-import DebugConfig from '../Config/DebugConfig'
-import React, { Component } from 'react'
-import { Provider } from 'react-redux'
-import { View } from 'react-native'
+import '../Config';
+import DebugConfig from '../Config/DebugConfig';
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { View } from 'react-native';
 import { Toast } from 'react-native-redux-toast';
-import RootContainer from './RootContainer'
-import createStore from '../Redux'
+import RootContainer from './RootContainer';
+import createStore from '../Redux';
+import OneSignal from 'react-native-onesignal'; // Import package from node modules
+
 
 // create our store
 const store = createStore()
@@ -20,6 +22,37 @@ const store = createStore()
  * We separate like this to play nice with React Native's hot reloading.
  */
 class App extends Component {
+
+  constructor(properties) {
+    super(properties);
+    OneSignal.init("abc8864e-a22d-4635-8969-b08ef7da58c6");
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log("Notification received: ", notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
   render() {
     return (
       <Provider store={store}>
--- android/app/src/main/java/com/sunilnaik/MainApplication.java
+++ android/app/src/main/java/com/sunilnaik/MainApplication.java
@@ -3,6 +3,7 @@ package com.sunilnaik;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.geektime.rnonesignalandroid.ReactNativeOneSignalPackage;
 import fr.bamlab.rnimageresizer.ImageResizerPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.imagepicker.ImagePickerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeOneSignalPackage(),
             new ImageResizerPackage(),
             new RNGestureHandlerPackage(),
             new ImagePickerPackage(),
