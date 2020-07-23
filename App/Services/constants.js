ca345293e980aa8ce8420d3d7ccecc8f2c572b2c
Lalith Narayan Kashyap <lalith@recruiterbox.com>
2020-07-23 22:45:38 +0530

    Surya demo

--- App/Services/constants.js
+++ App/Services/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://sunilnaikmla.com/api/';
+export const BASE_URL = 'http://zonemanager.ml.com/api/';
 export const API_VERSION = 'v1/';
 export const APP_TOKEN = "acb438035c2823a9c6da565cfa78d7b8029c32db7cf55d4bb1442ca4c51e602f";
 
